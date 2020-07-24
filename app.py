25222ffc634185645fe140d1361efe857754051e
Paras Gulati <44550739+parasgulati@users.noreply.github.com>
2020-07-24 23:51:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,20 @@ from flask import Flask,request
 from model import predict
 
 app = Flask(__name__)
+
+class LogisticRegressionModel(Resource):
+  def get(self):
+    predictQuery={}
+    listColumns=t.columns.tolist()
+    for i in listColumns:
+      predictQuery[i]=request.args.get(i)
+    answer=predictLogisticModel(predictQuery)
+    return {'Attrition':answer}
+
+
+app.add_resource(LogisticRegressionModel, '/predictLogisticRegression')
+
+'''
 @app.route('/')
 
 def index():
@@ -24,6 +38,6 @@ def predict():
     else:
         prediction = model.predict.predictRandomForestModel(data) 
     return json.dumps(str(prediction))
-
+'''
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
