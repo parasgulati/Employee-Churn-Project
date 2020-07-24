b48004e6682e067f8005804d5f3fc056dc955dbd
Paras Gulati <44550739+parasgulati@users.noreply.github.com>
2020-07-24 23:54:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 class LogisticRegressionModel(Resource):
   def get(self):
+    print('Hi in logisticRegressionModelGet')
     predictQuery={}
     listColumns=t.columns.tolist()
     for i in listColumns:
