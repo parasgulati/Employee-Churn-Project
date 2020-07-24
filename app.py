aeaa6616fea343b5d0d76649d8715421edcf9579
Paras Gulati <44550739+parasgulati@users.noreply.github.com>
2020-07-25 00:08:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,39 +3,19 @@ from model import predict
 
 app = Flask(__name__)
 
-class LogisticRegressionModel(Resource):
-  def get(self):
-    print('Hi in logisticRegressionModelGet')
-    predictQuery=request.args.get('Data')
-    answer=predictLogisticModel(predictQuery)
-    return {'Attrition':answer}
-
-
-app.add_resource(LogisticRegressionModel, '/predictLogisticRegression')
-
-'''
-@app.route('/')
-
-def index():
-    return "Index Page"
-
 @app.route('/predictLogistic',methods=['GET','POST'])
 def predict():
-    data = request.form.get('data')
-    if data == None:
-        return 'Got None'
-    else:
-        prediction = model.predict.predictLogisticModel(data) 
+  if request.method=='POST':
+    data=request.form['data']
+    prediction = model.predict.predictLogisticModel(data)
     return json.dumps(str(prediction))
-
+  
 @app.route('/predictRandomForest',methods=['GET','POST'])
 def predict():
-    data = request.form.get('data')
-    if data == None:
-        return 'Got None'
-    else:
-        prediction = model.predict.predictRandomForestModel(data) 
+  if request.method=='POST':
+    data=request.form['data']
+    prediction = model.predict.predictRandomForestModel(data) 
     return json.dumps(str(prediction))
-'''
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True)
+  app.run(host='0.0.0.0',debug=True)
